ba35ab58c0a34571b7dfd4ce9edf012abb12e2be
ChristopherCornford <ccornford02906@gmail.com>
2017-06-20 15:25:44 -0400

    Fuck Incontrol, my way is easier

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,17 +4,36 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
+
 	public GameObject movement;
 	public int m_playerNumber = 0;
 	public float speed = 12f;
 	public float turnSpeed = 180f;
 
 
-	public string movementAxisName;
-	public string turnAxisName;
+	public string movementKeyAxisName;
+	public string strafeKeyAxisName;
+	public string turnKeyAxisName;
+	public string aimKeyAxisName;
+	public string actionKeyAxisName;
+	public string dashKeyAxisName;
+
+	public string movementJoyAxisName;
+	public string strafeJoyAxisName;
+	public string turnJoyAxisName;
+	public string aimJoyAxisName;
+	public string actionJoyAxisName;
+	public string dashJoyAxisName;
+
 	private Rigidbody rb;
-	private float movementInputValue;
-	private float turnInputValue;
+
+	private float movementKeyInputValue;
+	private float strafeKeyInputValue;
+	private float turnKeyInputValue;
+
+	private float movementJoyInputValue;
+	private float strafeJoyInputValue;
+	private float turnJoyInputValue;
 
 	private void Awake () {
 		
@@ -26,55 +45,88 @@ public class PlayerMovement : MonoBehaviour {
 
 		rb.isKinematic = false;
 
-		movementInputValue = 0f;
-		turnInputValue = 0f;
+		movementKeyInputValue = 0f;
+		strafeKeyInputValue = 0f;
+		turnKeyInputValue = 0f;
 
+		movementJoyInputValue = 0f;
+		strafeJoyInputValue = 0f;
+		turnJoyInputValue = 0f;
 	}
 
 	private void OnDisable () {
 
 		rb.isKinematic = true;
+	
 	}
 
 	private void Start () {
 
+		movementKeyAxisName = "Player" + m_playerNumber + "KeyMove";
+		strafeKeyAxisName = "Player" + m_playerNumber + "KeyStrafe";
+		turnKeyAxisName = "Player" + m_playerNumber + "KeyTurn";
+		aimKeyAxisName = "Player" + m_playerNumber + "KeyAim";
+		actionKeyAxisName = "Player" + m_playerNumber + "KeyAction";
+		dashKeyAxisName = " Player" + m_playerNumber + "KeyDash";
 
+		movementJoyAxisName = "Player" + m_playerNumber + "JoyMove";
+		strafeJoyAxisName = "Player" + m_playerNumber + "JoyStrafe";
+		turnJoyAxisName = "Player" + m_playerNumber + "JoyTurn";
+		aimJoyAxisName = "Player" + m_playerNumber + "JoyAim";
+		actionJoyAxisName = "Player" + m_playerNumber + "JoyAction";
+		dashJoyAxisName = " Player" + m_playerNumber + "JoyDash";
 
-		movementAxisName = "Vertical" + m_playerNumber;
-		turnAxisName = "Horizontal" + m_playerNumber;
-
-		print (movementAxisName);
-		print (turnAxisName);
 	}
 
 	private void Update () {
 
-		movementInputValue = Input.GetAxisRaw (movementAxisName);
-		turnInputValue = Input.GetAxisRaw (turnAxisName);
+		movementKeyInputValue = Input.GetAxisRaw (movementKeyAxisName);
+		strafeKeyInputValue = Input.GetAxisRaw (strafeKeyAxisName);
+		turnKeyInputValue = Input.GetAxisRaw (turnKeyAxisName);
+
+		movementJoyInputValue = Input.GetAxisRaw (movementJoyAxisName);
+		strafeJoyInputValue = Input.GetAxisRaw(strafeJoyAxisName);
+		turnJoyInputValue = Input.GetAxisRaw (turnJoyAxisName);
 
 	}
 
 	private void FixedUpdate () {
+
+
 	
 		Move ();
 		Turn ();
+		if 
 
 	}
 
 	private void Move() {
 
-		Vector3 movement = transform.forward * movementInputValue * speed * Time.deltaTime;
+		Vector3 keyMovement = transform.forward * movementKeyInputValue * speed * Time.deltaTime;
+		Vector3 keyStrafe = transform.right * strafeKeyInputValue * speed * Time.deltaTime;
 
-		rb.MovePosition (rb.position + movement);
+		Vector3 joyMovement = transform.forward * -movementJoyInputValue * speed * Time.deltaTime;
+		Vector3 joyStrafe = transform.right * strafeJoyInputValue * speed * Time.deltaTime;;
+
+		rb.MovePosition (rb.position + keyMovement);
+		rb.MovePosition (rb.position + keyStrafe);
+		rb.MovePosition (rb.position + joyMovement);
+		rb.MovePosition (rb.position + joyStrafe);
 	}
 
 	private void Turn () {
 
-		float turn = turnInputValue * turnSpeed * Time.deltaTime;
+		float keyTurn = turnKeyInputValue * turnSpeed * Time.deltaTime;
 
-		Quaternion turnRotation = Quaternion.Euler (0f, turn, 0f);
+		Quaternion keyTurnRotation = Quaternion.Euler (0f, keyTurn, 0f);
 
-		rb.MoveRotation (rb.rotation * turnRotation);
+		rb.MoveRotation (rb.rotation * keyTurnRotation);
+
+		float joyTurn = turnJoyInputValue * turnSpeed * Time.deltaTime;
+
+		Quaternion joyTurnRotation = Quaternion.Euler (0f, joyTurn, 0f);
+
+		rb.MoveRotation (rb.rotation * joyTurnRotation);
 	}
 }
 
