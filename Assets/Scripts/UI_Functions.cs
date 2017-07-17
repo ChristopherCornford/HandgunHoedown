a1b588c0ea8c12db24bf804d54cb65d8bd95b269
Ethan Thibault <wickedlystudios@gmail.com>
2017-07-17 12:54:50 -0400

    MainMenu Layout, UI Functions
    - Deleted ConsolE (outdated due to 2017.1)
    - Created UI Functions.cs, contains all functions for main menu functionality
    - Created Main Menu Scene and decorated it prefab of desert environment
    - Put Main Menu in BuildIndex

---
+++ Assets/Scripts/UI_Functions.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class UI_Functions : MonoBehaviour {
+
+	[HeaderAttribute("UI Panels")]
+	public GameObject LevelSelect_Panel;
+	public GameObject Controls_Panel;
+	public GameObject Credits_Panel;
+	[HeaderAttribute("Level Button to Focus")]
+	public GameObject Level1Button;
+
+	// Main Menu Buttons
+	public void LevelSelect(){
+		LevelSelect_Panel.SetActive(true);
+		EventSystem.current.SetSelectedGameObject(Level1Button);
+	}
+	public void Controls(){
+		Controls_Panel.SetActive(true);
+	}
+	public void Credits(){
+		Credits_Panel.SetActive(true);
+	}
+	public void Quit(){
+		Application.Quit();
+	}
+
+	// Level Select Buttons
+	/* Create all the level button functions here
+	SceneManager.LoadScene(name of scene or int in build index) */
+
+}
