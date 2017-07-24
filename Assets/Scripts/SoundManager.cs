2447f6c6e05015a9bde3659a82f64e2d88892d0a
Ethan Thibault <wickedlystudios@gmail.com>
2017-07-24 15:37:53 -0400

    New Sounds, Starting SoundManager

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+	private AudioSource thisSource;
+	void Awake () {
+		thisSource = this.GetComponent<AudioSource>();;
+	}
+	
+	// public void 
+}
