52879557d57391b33a6e52db1ce7802b9dace7bc
Ethan Thibault <wickedlystudios@gmail.com>
2017-08-17 20:26:57 -0400

    Set cursor to non-visible
    - Also fixed padding on pause menu

--- Assets/Scripts/UI_Manager.cs
+++ Assets/Scripts/UI_Manager.cs
@@ -36,6 +36,7 @@ public class UI_Manager : MonoBehaviour {
 	private Color none = new Color32(0,0,0,0);
 
 	void Start(){
+		Cursor.visible = false;
 		EventSystem.current.firstSelectedGameObject = HowtoPlay;
 	}
 
